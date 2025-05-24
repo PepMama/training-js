668be83723aeee3d6244a723b6be4fa9c26fbdad
Maelic Pepin <pmaelic@outlook.fr>
2025-05-24 17:11:51 +0200

    PM - :sparkles: Feat: javascript to calculate BMI

--- 1. IMC/app.js
+++ 1. IMC/app.js
@@ -7,4 +7,43 @@ const BMIData = [
   { name: "Obésité morbide", color: "purple", range: 40 },
 ];
 
+const form = document.querySelector('form');
+const input = document.querySelectorAll('input');
+const displayBMI = document.querySelector(".bmi-value");
+const result = document.querySelector(".result");
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  calculateImc();
+});
+
+function calculateImc() {
+  const length = input[0].value;
+  const weight = input[1].value;
+
+  if (!length || !weight || length <= 0 || weight <= 0) {
+    console.error("Impossible de calculer")
+    displayBMI.textContent = "Impossible de calculer";
+    result.textContent = "Veuillez entrer des bonnes valeurs de taille ou de poids !";
+    return;
+  }
+
+  const IMC = (weight / Math.pow(length / 100, 2)).toFixed(1);
+  console.log(IMC);
+
+  showResult(IMC);
+}
+
+function showResult(BMI) {
+  const rank = BMIData.find(data => {
+    if (BMI >= data.range[0] && BMI < data.range[1]) return data;
+    else if (typeof data.range === "number" && BMI >= data.range) return data;
+  });
+
+  displayBMI.textContent = BMI;
+  displayBMI.style.color = `${rank.color}`;
+  result.textContent = `Résultat : ${rank.name}`;
+}
+
 // IMC = poids en kg / taille² en m
